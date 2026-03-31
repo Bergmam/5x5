85e6a3a22909d3ecb7c5e9f219870d2b75c9c2de
Anton Bergman <antonbargman@gmail.com>
2026-03-31 20:37:01 +0900

    feat: add ci, gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'node',
-    globals: true,
-    coverage: {
-      reporter: ['text']
-    }
-  }
+  base: '/5x5/',
 });
